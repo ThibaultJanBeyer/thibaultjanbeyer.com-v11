8ed99707b0498d0f4f63adb6f4d67d05873c592b
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2019-05-04 14:25:33 +0200

    fix github stars accessibility issue

--- src/assets/javascript/projects-dragndrop.js
+++ src/assets/javascript/projects-dragndrop.js
@@ -1,7 +1,7 @@
 fetch("//api.github.com/repos/thibaultjanbeyer/dragndrop")
   .then(res => res.json())
   .then(res => {
-    document.querySelector("#dragndrop-gh-stars").innerHTML =
+    document.querySelector("#dragndrop-gh-stars").firstChild.innerHTML =
       res.stargazers_count;
   })
   .catch(err => console.log(err));
--- src/assets/javascript/projects-dragselect.js
+++ src/assets/javascript/projects-dragselect.js
@@ -1,7 +1,7 @@
 fetch("//api.github.com/repos/thibaultjanbeyer/dragselect")
   .then(res => res.json())
   .then(res => {
-    document.querySelector("#dragselect-gh-stars").innerHTML =
+    document.querySelector("#dragselect-gh-stars").firstChild.innerHTML =
       res.stargazers_count;
   })
   .catch(err => console.log(err));
